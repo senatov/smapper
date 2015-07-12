3b9bd17f3149127f598f6af362475e996bf16d58
Iakov <Iakov@Magadan>
2015-07-12 23:16:32 +0200

    ~ on work: log4j logging on /log

--- com.senatov.smapperApp/src/main/java/com/senatov/smapperApp/parts/model/FileTreeLabelProvider.java
+++ com.senatov.smapperApp/src/main/java/com/senatov/smapperApp/parts/model/FileTreeLabelProvider.java
@@ -1,4 +1,16 @@
+/*
+ * JDK: 1.8.0_45
+ * @author I. Senatov (Iakov)
+ * DATE: 12.07.2015 22:36:11
+ * PRJ: com.senatov.smapperApp
+ * PACKAGE:com.senatov.smapperApp.parts.model
+ * FILE: FileTreeLabelProvider.java
+ */
+
+
 package com.senatov.smapperApp.parts.model;
+
+
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -6,64 +18,123 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.swt.graphics.Image;
 
+
+
+/**
+ * The Class FileTreeLabelProvider.
+ */
 public class FileTreeLabelProvider implements ILabelProvider {
-	private final List listeners;
-
+	
+	private static final Logger LOG = Logger.getLogger(FileTreeLabelProvider.class );
+	private final List<ILabelProviderListener> listeners;
 	private Image file;
-
 	private Image dir;
-
+	
+	
+	
+	/**
+	 * Instantiates a new file tree label provider.
+	 */
 	public FileTreeLabelProvider() {
-		listeners = new ArrayList();
-
+		LOG.debug("FileTreeLabelProvider ()" );
+		listeners = new ArrayList<ILabelProviderListener>();
 		try {
-			file = new Image(null, new FileInputStream("images/file.gif"));
-			dir = new Image(null, new FileInputStream("images/directory.gif"));
-		} catch (FileNotFoundException e) {
+			file = new Image(null, new FileInputStream("images/file.gif" ) );
+			dir = new Image(null, new FileInputStream("images/directory.gif" ) );
+		}
+		catch(final FileNotFoundException e ) {
+			LOG.error(e.getMessage() );
 		}
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ILabelProvider#getImage(java.lang.Object)
+	 */
 	@Override
-	public Image getImage(Object arg0) {
-		return ((File) arg0).isDirectory() ? dir : file;
+	public Image getImage(Object arg0 ) {
+		
+		LOG.debug("getImage )" );
+		return ((File ) arg0 ).isDirectory() ? dir : file;
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
+	 */
 	@Override
-	public String getText(Object arg0) {
-		String text = ((File) arg0).getName();
-
-		if (((File) arg0).getName().length() == 0) {
-			text = ((File) arg0).getPath();
+	public String getText(Object arg0 ) {
+		
+		LOG.debug("getText" );
+		String text = ((File ) arg0 ).getName();
+		if(((File ) arg0 ).getName().length() == 0 ) {
+			text = ((File ) arg0 ).getPath();
 		}
 		return text;
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#addListener(org.eclipse.jface.viewers.
+	 * ILabelProviderListener)
+	 */
 	@Override
-	public void addListener(ILabelProviderListener arg0) {
-		listeners.add(arg0);
+	public void addListener(ILabelProviderListener arg0 ) {
+		
+		listeners.add(arg0 );
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#dispose()
+	 */
 	@Override
 	public void dispose() {
+		
 		// Dispose the images
-		if (dir != null)
+		if(dir != null ) {
 			dir.dispose();
-		if (file != null)
+		}
+		if(file != null ) {
 			file.dispose();
+		}
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(java.lang.Object, java.lang.String)
+	 */
 	@Override
-	public boolean isLabelProperty(Object arg0, String arg1) {
+	public boolean isLabelProperty(Object arg0, String arg1 ) {
+		
 		return false;
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#removeListener(org.eclipse.jface.viewers.
+	 * ILabelProviderListener)
+	 */
 	@Override
-	public void removeListener(ILabelProviderListener listener) {
+	public void removeListener(ILabelProviderListener listener ) {
 		// Auto-generated method stub
-
+		
 	}
 }
--- com.senatov.smapperApp/src/main/java/com/senatov/smapperApp/parts/model/TreeContentProvider.java
+++ com.senatov.smapperApp/src/main/java/com/senatov/smapperApp/parts/model/TreeContentProvider.java
@@ -1,38 +1,99 @@
+/*
+ * JDK: 1.8.0_45
+ * @author I. Senatov (Iakov)
+ * DATE: 12.07.2015 22:36:11
+ * PRJ: com.senatov.smapperApp
+ * PACKAGE:com.senatov.smapperApp.parts.model
+ * FILE: TreeContentProvider.java
+ */
+
+
 package com.senatov.smapperApp.parts.model;
+
+
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
+
+
+// TODO: Auto-generated Javadoc
 public class TreeContentProvider implements ITreeContentProvider {
+	
+	private static final Logger LOG = Logger.getLogger(TreeContentProvider.class );
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+	 */
 	@Override
-	public Object[] getChildren(Object arg0) {
-		return ((File) arg0).listFiles();
+	public Object[] getChildren(Object arg0 ) {
+		
+		return ((File ) arg0 ).listFiles();
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+	 */
 	@Override
-	public Object getParent(Object arg0) {
-		return ((File) arg0).getParentFile();
+	public Object getParent(Object arg0 ) {
+		
+		return ((File ) arg0 ).getParentFile();
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+	 */
 	@Override
-	public boolean hasChildren(Object arg0) {
-		Object[] obj = getChildren(arg0);
+	public boolean hasChildren(Object arg0 ) {
+		
+		Object[] obj = getChildren(arg0 );
 		return obj == null ? false : obj.length > 0;
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getElements(java.lang.Object)
+	 */
 	@Override
-	public Object[] getElements(Object arg0) {
+	public Object[] getElements(Object arg0 ) {
+		
 		return File.listRoots();
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
 	@Override
 	public void dispose() {
+	
 	}
-
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
+	 * java.lang.Object, java.lang.Object)
+	 */
 	@Override
-	public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+	public void inputChanged(Viewer arg0, Object arg1, Object arg2 ) {
+	
 	}
-
 }

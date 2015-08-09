b688585b90c40d87f2c9f7ccfdacb700de6033e3
senatov <senatov@gmail.com>
2015-08-09 18:43:43 +0200

    add to branch 1.0.8

---
+++ plugins/com.senatov.smapper.smapperApp/src/main/java/com/senatov/smapper/parts/SamplePart.java
@@ -0,0 +1,123 @@
+/*
+ * JDK: 1.8.0_45
+ * @author I. Senatov (Iakov)
+ * DATE: 23.07.2015 12:47:52
+ * PRJ: com.senatov.smapperApp
+ * PACKAGE:com.senatov.smapperApp.parts
+ * FILE: SamplePart.java
+ */
+
+
+package com.senatov.smapperApp.parts;
+
+
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.log4j.Logger;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+
+import com.senatov.smapperApp.parts.model.FileTreeLabelProvider;
+import com.senatov.smapperApp.parts.model.TreeContentProvider;
+
+
+
+/**
+ * The Class SamplePart.
+ */
+public class SamplePart {
+
+	private static final Logger LOG = Logger.getLogger(SamplePart.class );
+
+
+
+	/**
+	 * Creates the composite.
+	 *
+	 * @param parent
+	 *            the parent
+	 * @throws IOException
+	 */
+	@PostConstruct
+	public void createComposite(Composite parent ) {
+
+		LOG.debug("createComposite()" );
+		final GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		// 1
+		createLTree(parent );
+		// 2
+		createRTree(parent );
+		parent.setLayout(gridLayout );
+	}
+
+
+
+	/**
+	 * Creates the first tree.
+	 *
+	 * @param pParent the parent
+	 */
+	private void createLTree(Composite pParent ) {
+
+		LOG.debug("createTree()" );
+		final Composite composite = new Composite(pParent, SWT.BAR );
+		composite.setLayout(new GridLayout(1, false ) );
+		final CheckboxTreeViewer pTreeViewer = new CheckboxTreeViewer(composite );
+		pTreeViewer.setAutoExpandLevel(2 );
+		pTreeViewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH ) );
+		pTreeViewer.setContentProvider(new TreeContentProvider() );
+		pTreeViewer.setLabelProvider(new FileTreeLabelProvider() );
+		pTreeViewer.setInput("root" ); // pass a non-null that will be ignored
+		// When user checks a checkbox in the tree, check all its children
+		pTreeViewer.addCheckStateListener(event -> {
+			if(event.getChecked() ) {
+				pTreeViewer.setSubtreeChecked(event.getElement(), true );
+			}
+		} );
+	}
+
+
+
+	/**
+	 * Creates the first tree.
+	 *
+	 * @param pParent the parent
+	 */
+	private void createRTree(Composite pParent ) {
+
+		LOG.debug("createRTree()" );
+		final Composite composite = new Composite(pParent, SWT.BAR );
+		composite.setLayout(new GridLayout(1, false ) );
+		final CheckboxTreeViewer pTreeViewer = new CheckboxTreeViewer(composite );
+		pTreeViewer.setAutoExpandLevel(2 );
+		pTreeViewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH ) );
+		pTreeViewer.setContentProvider(new TreeContentProvider() );
+		pTreeViewer.setLabelProvider(new FileTreeLabelProvider() );
+		pTreeViewer.setInput("root" ); // pass a non-null that will be ignored
+		// When user checks a checkbox in the tree, check all its children
+		pTreeViewer.addCheckStateListener(event -> {
+			if(event.getChecked() ) {
+				pTreeViewer.setSubtreeChecked(event.getElement(), true );
+			}
+		} );
+	}
+
+
+
+	/**
+	 * Sets the focus.
+	 */
+	@Focus
+	public void setFocus() {
+
+		LOG.debug("setFocus()" );
+	}
+}
---
+++ plugins/com.senatov.smapper.smapperApp/src/main/java/com/senatov/smapper/parts/model/TreeContentProvider.java
@@ -0,0 +1,103 @@
+/*
+ * JDK: 1.8.0_45
+ * @author I. Senatov (Iakov)
+ * DATE: 23.07.2015 01:39:36
+ * PRJ: com.senatov.smapperApp
+ * PACKAGE:com.senatov.smapperApp.parts.model
+ * FILE: TreeContentProvider.java
+ */
+
+
+package com.senatov.smapperApp.parts.model;
+
+
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+
+
+// TODO: Auto-generated Javadoc
+public class TreeContentProvider implements ITreeContentProvider {
+	
+	private static final Logger LOG = Logger.getLogger(TreeContentProvider.class );
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+	 */
+	@Override
+	public Object[] getChildren(Object arg0 ) {
+		
+		return ((File ) arg0 ).listFiles();
+	}
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+	 */
+	@Override
+	public Object getParent(Object arg0 ) {
+		
+		return ((File ) arg0 ).getParentFile();
+	}
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+	 */
+	@Override
+	public boolean hasChildren(Object arg0 ) {
+		
+		boolean bRet = false;
+		Object[] obj = getChildren(arg0 );
+		if(obj != null && obj.length > 0 ) {
+			bRet = true;
+		}
+		return bRet;
+	}
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getElements(java.lang.Object)
+	 */
+	@Override
+	public Object[] getElements(Object arg0 ) {
+		
+		return File.listRoots();
+	}
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+	
+	}
+	
+	
+	
+	/*
+	 * <br><br><b>author</b> iase27698054 2015-03-15
+	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
+	 * java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public void inputChanged(Viewer arg0, Object arg1, Object arg2 ) {
+	
+	}
+}

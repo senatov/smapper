f21e7c87355a65ab6d2cfda911a93bbb9b751363
senatov <senatov@gmail.com>
2015-08-11 15:50:28 +0200

    + junit lib

---
+++ plugins/com.senatov.smapper.smapperApp/src/test/java/com/senatov/smapperApp/handlers/FindHandlerTest.java
@@ -0,0 +1,77 @@
+/*
+ * JDK: JDK 1.8_51
+ * @author Iakov
+ * DATE: 11.08.2015 11:53:22 2015 
+ * PRJ: com.senatov.smapperApp
+ * PACKAGE: com.senatov.smapperApp.handlers
+ * FILE: FindHandlerTest.java
+ */
+package com.senatov.smapperApp.handlers;
+
+
+
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+/**
+ * The Class FindHandlerTest.
+ */
+public class FindHandlerTest {
+
+	/**
+	 * Sets the up before class.
+	 *
+	 * @throws Exception the exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+
+	/**
+	 * Tear down after class.
+	 *
+	 * @throws Exception the exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+
+	/**
+	 * Sets the up.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+	/**
+	 * Tear down.
+	 *
+	 * @throws Exception the exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	/**
+	 * Test execute.
+	 */
+	@Test
+	public void testExecute() {
+		assertTrue("Not yet implemented", true);
+	}
+
+}

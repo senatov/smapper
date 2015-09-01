bc93052154cc877ff6ef97333d8d6c65bfa693d3
senatov <senatov@gmail.com>
2015-09-01 08:52:45 +0200

    + target  +handle class

---
+++ com.senatov.smapper.plugin/src/main/java/org/eclipse/ui/navigator/CommonNavigator.java
@@ -0,0 +1,24 @@
+package org.eclipse.ui.navigator;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CommonNavigator extends ViewPart {
+
+	public CommonNavigator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

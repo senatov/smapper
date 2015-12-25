215f8506cc4e22647415882d1f4d870628707552
Iakov <senatov@gmail.com>
2015-12-25 04:28:00 +0100

    works again

---
+++ smapper.plugin1/src/main/java/com/senatov/smapperApp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,14 @@
+package com.senatov.smapperApp;
+
+import org.eclipse.ui.application.WorkbenchAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+

905b82330e7168edc758883e9a98cfd0bafaefa7
senatov <javaentwickler@gmail.com>
2016-07-03 03:33:59 +0200

    ~ 4.6 release platform

---
+++ smapper.plugin1/src/main/java/com/senatov/smapper/dialogs/ContenDirector.java
@@ -0,0 +1,20 @@
+package com.senatov.smapper.dialogs;
+
+import org.eclipse.ui.intro.IntroContentDetector;
+
+
+public class ContenDirector extends IntroContentDetector {
+
+	public ContenDirector() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	@Override
+	public boolean isNewContentAvailable() {
+
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
